6d2af5f0ffbfa645ef4a5628e7403accc07ac08a
lyncir <lyncir@gmail.com>
2018-04-04 09:29:28 +0800

    update config.py

--- config.py
+++ config.py
@@ -6,12 +6,3 @@ DEBUG = False
 # RESPONSE_TIMEOUT = 2  # Default 60
 # KEEP_ALIVE = True  # Default True
 # KEEP_ALIVE_TIMEOUT = 5  # Default 5
-
-# redis config
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_POOLSIZE = 10
-
-# admin
-USERNAME = 'admin'
-PASSWORD = '123456'
